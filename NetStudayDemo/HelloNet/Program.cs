7d9e461b515509d15ac9354b6e727fb2930af08f
guansheng <guansheng@GUANSHEN187>
2017-04-08 23:47:38 +0800

    第一天的案例

---
+++ NetStudayDemo/HelloNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //输出 Hello Word
+            Console.WriteLine("Hello Word");
+            //输出问候语句
+            Console.WriteLine("你好世界！");
+            //暂停程序的执行，等待用户的输入
+            Console.ReadLine();
+            
+        }
+    }
+}
